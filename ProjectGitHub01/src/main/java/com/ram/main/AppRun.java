e68380507f1ca6fce3fe5705bc429c9a8edf0a01
vrach <vrach@192.168.1.31>
2024-07-12 22:19:33 +0530

    Dev: test cases and main method chaged

--- ProjectGitHub01/src/main/java/com/ram/main/AppRun.java
+++ ProjectGitHub01/src/main/java/com/ram/main/AppRun.java
@@ -1,9 +1,14 @@
 package com.ram.main;
 
+import com.ram.service.ArithmeticOperation;
+
 public class AppRun 
 {
+	
 public static void main(String[] args) 
 {
-	
+	System.out.println("AppRun.main()");
+	ArithmeticOperation op=new ArithmeticOperation();
+	System.out.println(op.sum(20,30));;
 }
 }
--- ProjectGitHub01/src/test/java/com/ram/TestArithmetic.java
+++ ProjectGitHub01/src/test/java/com/ram/TestArithmetic.java
@@ -9,11 +9,30 @@ import com.ram.service.ArithmeticOperation;
 public class TestArithmetic 
 {
 	@Test
-public void testSum()
-{
-	ArithmeticOperation a=new ArithmeticOperation();
-	int actual=a.sum(100, 10);
-	int expected=110;
-	assertEquals(expected,actual);
-}
+	public void testSumWithPositives()
+	{
+		
+		
+		
+		ArithmeticOperation a=new ArithmeticOperation();
+		int actual=a.sum(100, 10);
+		int expected=110;
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testSumWithNegatives()
+	{
+		ArithmeticOperation a=new ArithmeticOperation();
+		int actual=a.sum(-234,-210);
+		int expected=-444;
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testSumWithZero()
+	{
+		ArithmeticOperation a=new ArithmeticOperation();
+		int actual=a.sum(0, 0);
+		int expected=0;
+		assertEquals(expected,actual);
+	}
 }
